75cc9939338ea07a0d3ee68719e3474a5ccad08d
Vatsal <77259852+V4TSAL@users.noreply.github.com>
2022-11-16 13:03:20 +0530

    Time: 87 ms (91.28%), Space: 21.2 MB (97.92%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countNodes(self, root, l=1, r=1):
+
+        if not root : return 0
+        
+        left = right = root                           # compute both left and right heights of
+        while left  := left.left   : l += 1           # each subtree by going all way down to
+        while right := right.right : r += 1           # the left and right (in logN time)
+
+        if l == r : return 2**l - 1                   # if it's a full tree, its size is known
+        
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
