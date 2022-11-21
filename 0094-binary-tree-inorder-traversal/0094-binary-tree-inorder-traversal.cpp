77cba8bbf69057d72cf6154f4b6b21211291b78e
Vatsal <77259852+V4TSAL@users.noreply.github.com>
2022-11-21 12:03:31 +0530

    Time: 3 ms (63.27%), Space: 8.3 MB (75.67%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,25 +1,28 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        l=[]
-        if root is None:
-            return []
-        else:
-            return self.inorderTraversal(root.left)+[root.val]+ self.inorderTraversal(root.right)
-            # l.append()
-            # l.append()
-        # print(l)
-        # return (l)
-        # for i in l:
-        #     if i.isdigit():
-        #         answer.append(i)
-        # return answer
-        
-            
-    # def helper( self , )
+class Solution {
+public:
+vector<int> inorderTraversal(TreeNode* root) {
+    vector<int>ans;
+    while(root){
+        if(!root->left){
+            ans.push_back(root->val);
+            root=root->right;
+        }
+        else{
+            TreeNode* r=root->left;
+            while(r->right && r->right!=root){
+                r=r->right;
+            }
+            if(!r->right){
+                r->right=root;
+                root=root->left;
+            }
+            else{
+                r->right=NULL;
+                ans.push_back(root->val);
+                root=root->right;
+            }
+        }
+    }
+    return ans;
+}
+};
