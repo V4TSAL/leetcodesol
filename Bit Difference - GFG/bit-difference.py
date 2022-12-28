851140ab700874174b06d29da87021616d9ba1f9
Vatsal <77259852+V4TSAL@users.noreply.github.com>
2022-12-28 20:20:54 +0530

    Added solution - LeetHub

---
+++ Bit Difference - GFG/bit-difference.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+class Solution:
+    ##Complete this function
+    # Function to find number of bits needed to be flipped to convert A to B
+    def countBitsFlip(self,a,b):
+        s=a^b
+        c=0
+        while s:
+            s=s&(s-1)
+            c=c+1
+        return c
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+import math
+
+
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        ab=[int(x) for x in input().strip().split()]
+        a=ab[0]
+        b=ab[1]
+        ob=Solution()
+        print(ob.countBitsFlip(a,b))
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
