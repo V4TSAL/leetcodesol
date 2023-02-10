c1aae5f8501cc8a51254f2ac3e77028b21c32394
Vatsal <77259852+V4TSAL@users.noreply.github.com>
2023-02-10 19:42:29 +0530

    Time: 60 ms (56.26%), Space: 17.5 MB (94.23%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -1,9 +1,10 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-	    slow = fast = head #slow and fast pointers
-	    while fast and fast.next: #while not at the end of the list
-		    slow = slow.next #slow moves forward by 1
-		    fast = fast.next.next #fast moves forward by 2
-		    if slow == fast: #if they are the same then we must have a cycle!
-			    return True
-	    return False #no cycle found
+        a=head
+        b=head
+        while b and b.next:
+            a=a.next
+            b=b.next.next
+            if a==b:
+                return True
+        return False 
