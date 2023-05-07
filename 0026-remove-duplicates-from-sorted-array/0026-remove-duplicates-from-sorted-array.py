049d4e552eae5b172c27ccff258f7455aca546db
NIKHIL UPADHYAY <87298477+uniiik@users.noreply.github.com>
2023-05-07 17:28:40 +0530

    Update 0026-remove-duplicates-from-sorted-array.py
    i am give another approach to solve this question

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -9,6 +9,13 @@ class Solution:
             k=k+1
         return len(nums)
                 
+        /*hello vatsal, you can also create two pointers as i, j
+        initializing  to 1
+        Now can you can run a loop starting from 1 position and check whether the current element is equal to previos element , if it is then you can increase you value of j until this condition breaks and then you can 
+        place that jth index element in i-th position by this way your remova will be done in 1 iteration and inplace also.
+        If you like my contribution i would be we very happy
+        
+        */
             
             
             
