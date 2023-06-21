4dc491b25ce522e00bfd61435f155e801ebd1817
Vatsal <77259852+V4TSAL@users.noreply.github.com>
2023-06-21 12:02:50 +0530

    Time: 70 ms (44.34%), Space: 44.6 MB (69.22%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let c=init
+    return {
+        increment: ()=>c=c+1, decrement: ()=>c=c-1, reset: ()=>c=init
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
